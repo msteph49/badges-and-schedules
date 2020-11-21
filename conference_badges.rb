be26b714a0fe25a3d5534190335bd04b9711620c
Michael Stephens <mdstephens26@yahoo.com>
2020-11-21 16:59:53 -0500

    commit tree ab83afd836e800b3dd5af232e44584629c4ba10d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+require "pry"
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |attendees|
+        badges << "Hello, my name is #{attendees}."
+    end
+    return badges
+end
+
+
+def assign_rooms(attendees)
+    rooms = []
+    attendees.each_with_index do |attendees, index|
+
+    rooms << "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"
+    end
+    return rooms
+
+end
+
+
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|badge| puts badge}
+    assign_rooms(attendees).each{|rooms| puts rooms}
+  end
